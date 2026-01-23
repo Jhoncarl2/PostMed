532c1ab67d11f070e937890fa90ea775c11c9f55
jhoncarl2 <kacosvasquez14@gmail.com>
2026-01-22 22:59:16 -0400

    Estructura inicial de PostMed con Vite y React

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Reemplaza esto con tu clave real
+registerLicense('Ngo9BigBOggjGyl/VkR+XU9Ff1RAQmFIYVF2R2VJelRwcl9FaEwxOX1dQl9lSX9RdERkW3xfeHFWQGVXUkY=');
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
